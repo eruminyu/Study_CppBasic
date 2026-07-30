cc029873432bfb6b7704ab34db78668889e09573
Serian <eruminyu@gmail.com>
2026-07-30 15:12:04 +0900

    Day4
    가상 함수 내용 진행

--- ClassInheritance/Main.cpp
+++ ClassInheritance/Main.cpp
@@ -12,6 +12,11 @@ public:
 	{
 		std::cout << "~Base() called\n";
 	}
+
+	void Play()
+	{
+
+	}
 };
 
 class Derived : public Base
@@ -26,10 +31,26 @@ public:
 		std::cout << "~Derived() called\n";
 	}
 
+	void Attack()
+	{
+
+	}
+
 };
 
 
 int main()
 {
-	Derived derived;
+	Base* b = new Derived();
+	// 자손 객체를 생성하면서 부모의 타입으로 생성, 당연히 됨.
+	b->Play();
+	//b->Attack();
+
+	delete b;
+	b = nullptr;
+
 }
+
+// 객체의 생성과정 소멸과정. 순서확인용
+// 생성은 부모 -> 자손
+// 소멸은 자손 -> 부모
