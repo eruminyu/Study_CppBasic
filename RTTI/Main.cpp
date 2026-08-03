648623d83e9cb434c82769b0a09c872941181445
Serian <eruminyu@gmail.com>
2026-08-03 17:48:26 +0900

    Day6
    6일차 이론수업 끝
    변형자(실습코드X) ,템플릿, RTTI(진행중)

---
+++ RTTI/Main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+class Actor
+{
+public:
+	virtual ~Actor() = default;
+};
+
+class Camera : public Actor
+{
+		
+};
+class Player : public Actor
+{
+};
+
+
+int main()
+{
+	// 업캐스팅은 안전하다.
+	Actor* actor1 = new Player();
+	Actor* actor2 = new Camera();
+
+	// 다운캐스팅은 안전하지 않다.
+	// RTTI를 사용하여 안전하게 다운캐스팅을 수행할 수 있다.
+	auto player = dynamic_cast<Player*>(actor1);
+	if (player)
+	{
+		std::cout << "Player로 캐스팅 성공!\n";
+		
+	}
+
+	const type_info& typeInfo1 = typeid(*actor1);
+	const type_info& typeInfoPlayer = typeid(Player);
+	const type_info& typeInfo2 = typeid(*actor2);
+
+	if (typeInfo1 == typeInfoPlayer)
+	{
+		std::cout << "actor1은 Player 타입입니다.\n";
+		//std::cout << typeInfo1.name() << "\n";
+		std::cout << typeInfo2.name() << "\n";
+	}
+	
+
+	std::cin.get();
+
+}
--- Template/Main.cpp
+++ Template/Main.cpp
@@ -1,60 +1,49 @@
 ﻿#include <iostream>
 
-
-// 두 수를 서로 교환하는 함수
-
+// 두 수를 서로 교환하는 함수.
 template<typename T>
-void Swap(T& a, T& b)
+void Swap(T & a, T & b)
 {
 	T temp = a;
 	a = b;
 	b = temp;
 }
 
-// 클래스 템플릿을 만들어보기
-
-template<typename T, int size = 10> // Size 라는 명확한 파라미터를 하나 지정 가능.
-// 두번째 파라미터를 지정하지 않으면 기본값으로 10이 들어감. (Default Parameter)
+// 클래스 템플릿.
+template<typename T, int size>
 class Data
 {
 public:
-	Data(const T& newData )
-		:data(newData)
-	{	//data = newData;
+	Data(const T& newData)
+		: data(newData)
+	{
+
 	}
 
-	T GetData() const
-	{
-		return data;
-	}
-	void SetData(const T& newData)
-	{
-		data = newData;
-	}
-	int GetSize() const
-	{
-		return size;
-	}
+	T GetData() const { return data; }
+	void SetData(const T& newData) { data = newData; }
+	int GetSize() const { return size; }
 
 private:
-
+	T data = {};
 };
 
 int main()
 {
 	int a = 10;
 	int b = 30;
-	Swap(a, b);
+	Swap<int>(a, b);
 
-	Data<int, 100> intData(100);
+	Data<int, 100> intData(30);
 	std::cout << intData.GetData() << "\n";
-	std::cout << "Size: " << intData.GetSize()
-		<< ", Data: " << intData.GetData() << "\n";
+	std::cout
+		<< "Size: " << intData.GetSize()
+		<< ", Data: " << intData.GetData()
+		<< "\n";
 
-	Data<float> pi(3.141592f);
+	Data<float, 5> pi(3.141592f);
 	std::cout << pi.GetData() << "\n";
-
-	std::swap<int>(a, b);
+	std::cout << pi.GetSize() << "\n";
 
 	std::cin.get();
 }
