dd32c02f611704528573f3e97e84792e4028f5a0
Serian <eruminyu@gmail.com>
2026-08-04 16:58:29 +0900

    Day7
    CustomRTTI, FileIO 실습

---
+++ CustomRTTI/CraftObject.h
@@ -0,0 +1,75 @@
+﻿#pragma once
+
+#include <memory>
+
+// 커스텀 RTTI를 위한 CraftObject 클래스 정의, 최상위, 기반
+class CraftObject
+{
+public:
+	// 현재 객체의 타입 아이디 반환 함수
+	// 순수 가상 함수(Pure Virtual Function)
+	// 로 정의되어, 파생 클래스에서 반드시 구현해야 함
+	virtual size_t GetType() const = 0;
+
+	// 타입 비교 함수
+	// CraftObject의 함수를 호출했다는 건 
+	// 타입이 다르다는 의미이므로 false 반환
+	virtual bool Is(size_t typeId) const
+	{
+		return false;
+	}
+
+	// 타입 질의 함수
+	template<typename T>
+		bool IsTypeOf() const
+	{
+		return Is(T::TypeId());
+	};
+
+};
+
+// 스마트 포인트 형변환 유틸 함수
+template<typename T, typename U> // 이럴 때 SFINAE를 적용할 수 있음. 
+//(SFINAE: Substitution Failure Is Not An Error)
+std::shared_ptr<T> Cast(const std::shared_ptr<U> object)
+{
+	// null 확인
+	if (!object)
+	{
+		return nullptr;
+	}
+
+	// object의 실제 타입이 T(또는 T의 하위) 타입인지 확인 후 형변환.
+	// 재귀적으로 처리할 것.
+	if (object->Is(T::TypeId()))
+	{
+		return std::static_pointer_cast<T>(object);
+	}
+	// 형변환 실패
+	return nullptr;
+
+}
+
+// 타입 시스템에 제공할 함수를 매크로로 구현
+#define TYPE_DECLARATIONS(Type, ParentType)							\
+	using super = ParentType;										\
+protected:															\
+	static size_t TypeIdClass()										\
+{																	\
+	static int runTimeTypeId = 0;									\
+	return reinterpret_cast<size_t>(&runTimeTypeId);				\
+}																	\
+public:																\
+	static size_t TypeId()											\
+	{																\
+		return Type::TypeIdClass();									\
+	}																\
+	virtual size_t GetType() const override							\
+	{																\
+		return Type::TypeIdClass();									\
+	}																\
+	virtual bool Is(size_t id) const override						\
+	{																\
+		return (id == TypeIdClass()) ? true : ParentType::Is(id);	\
+	}																\
+
---
+++ CustomRTTI/Main.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <string>
+#include "CraftObject.h"
+
+//
+//#define TO_NAME(x) #x
+//#define CLASS_NAME(type)					\
+//	public:									\
+//	virtual const char* ClassName() const	\
+//	{										\
+//		return TO_NAME(type);				\
+//	}
+//
+//class Actor
+//{ 
+////public:
+////	virtual const char* ClassName() const 
+////	{
+////		//return "Actor"; 
+////		return TO_NAME(Actor);
+////	}
+//
+//	CLASS_NAME(Actor)
+//
+//public:
+//	static size_t TypeId()
+//	{
+//		// 정적 지역 변수
+//		static int runTimeTypeId = 0;
+//		return reinterpret_cast<size_t>(&runTimeTypeId);
+//	}
+//};
+//
+//class Player : public Actor
+//{
+//private:
+//	CLASS_NAME(Player)
+//
+//public:
+//	static size_t TypeId()
+//	{
+//		// 정적 지역 변수
+//		static int runTimeTypeId = 0;
+//		return reinterpret_cast<size_t>(&runTimeTypeId);
+//	}
+//
+//};
+//
+
+class Actor : public CraftObject
+{
+using super = CraftObject; protected: static size_t TypeIdClass() {
+	static int runTimeTypeId = 0; return reinterpret_cast<size_t>(&runTimeTypeId);
+} public: static size_t TypeId() {
+	return Actor::TypeIdClass();
+} virtual size_t GetType() const override {
+	return Actor::TypeIdClass();
+} virtual bool Is(size_t id) const override {
+	return (id == TypeIdClass()) ? true : CraftObject::Is(id);
+}
+};
+
+class Player : public Actor
+{
+	TYPE_DECLARATIONS(Player, Actor)
+};
+
+class Camera : public Actor
+{
+	TYPE_DECLARATIONS(Camera, Actor)
+};
+
+
+int main()
+{
+	std::shared_ptr<Actor> camera = std::make_shared<Player>();
+	std::shared_ptr<Player> targerPlayer = Cast<Player>(camera);
+	if (targerPlayer)
+	{
+		std::cout << "형변환 성공\n";
+	}
+	else
+	{
+		std::cout << "형변환 실패\n";
+	}
+
+	//Actor* actor = new Actor();
+	//Actor* player = new Player();
+	//
+	//std::cout << actor->ClassName() << "\n";
+	//std::cout << player->ClassName() << "\n";
+
+	// 원시 문자열 비교
+	
+	//if (strcmp(actor->ClassName(), player->ClassName()) == 0)
+	//{
+	//	std::cout << "두 타입은 같습니다." << "\n";
+	//}
+	//else
+	//{
+	//	std::cout << "두 타입은 다릅니다." << "\n";
+	//}
+
+	//// TypeId 비교
+	//if (actor->TypeId() == Player::TypeId())
+	//{
+	//	std::cout << "두 타입은 같습니다." << "\n";
+	//}
+	//else
+	//{
+	//	std::cout << "두 타입은 다릅니다." << "\n";
+	//}
+	//
+	//delete actor;
+	//actor = nullptr;
+	//delete player;
+	//player = nullptr;
+}
