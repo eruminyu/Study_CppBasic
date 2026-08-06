6f17f5ceda4c84c830f0941f73434fd3268c658e
Serian <eruminyu@gmail.com>
2026-08-06 11:27:10 +0900

    오전 마무리
    오후에 계속

---
+++ UniquePtr/Main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <memory>
+
+class Actor
+{
+public:
+	virtual ~Actor() = default;
+};
+
+class Player : public Actor
+{
+public:
+	Player()
+	{
+		std::cout << " Player constructed.\n";
+	}
+	~Player()
+	{
+		std::cout << " Player destroyed.\n";
+	}
+
+};
+
+int main()
+{
+	Player* player = new Player();
+	delete player;
+	player = nullptr;
+
+}
